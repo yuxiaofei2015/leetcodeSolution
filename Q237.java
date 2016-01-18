b9bcacef9f369f9bddb8925ad3f9a001ac362c70
yuxiaofei <yuxiaofei_2015@163.com>
2016-01-19 00:52:37 +0800

    Q237

---
+++ Q237.java
@@ -0,0 +1,49 @@
+public class Q237 {
+
+	public static void main(String args[]) {
+		ListNode head = null, p = null;
+		for (int i = 1; i <= 10; i++) {
+			if (i == 1) {
+				head = p = new ListNode(1);
+			} else {
+				p.next = new ListNode(i);
+				p = p.next;
+			}
+		}
+		p = head;
+		for (int i = 0; i < 5; i++) {
+			p = p.next;
+		}
+		Q237 s = new Q237();
+		s.printlnList(head);
+		s.deleteNode(p);
+		s.printlnList(head);
+	}
+
+	public void deleteNode(ListNode node) {
+		if (node == null) {
+			return;
+		}
+		node.val = node.next.val;
+		node.next = node.next.next;
+	}
+
+	private void printlnList(ListNode node) {
+		ListNode p = node;
+		while (p != null) {
+			System.out.print(String.format("%d->", p.val));
+			p = p.next;
+		}
+		System.out.println("null");
+	}
+
+	private static class ListNode {
+		int val;
+		ListNode next;
+
+		public ListNode(int val) {
+			this.val = val;
+		}
+	}
+
+}
