20043852d8a607f91efbb5536bc6cb6e412c0da4
yuxiaofei <yuxiaofei_2015@163.com>
2016-01-23 01:44:37 +0800

    Q83

---
+++ solution/Q83.java
@@ -0,0 +1,17 @@
+package solution;
+
+import reference.ListNode;
+
+public class Q83 {
+	public ListNode deleteDuplicates(ListNode head) {
+		ListNode p = head;
+		while (p != null) {
+			ListNode temp = p;
+			while (temp.next != null && temp.val == temp.next.val) {
+				temp.next = temp.next.next;
+			}
+			p = p.next;
+		}
+		return head;
+	}
+}
