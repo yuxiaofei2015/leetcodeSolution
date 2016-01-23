24ff500ca20399493e58b8d7129686cc36a867f7
yuxiaofei <yuxiaofei_2015@163.com>
2016-01-23 08:44:43 +0800

    two esay problems

---
+++ solution/Q70.java
@@ -0,0 +1,17 @@
+package solution;
+
+public class Q70 {
+	public int climbStairs(int n) {
+		if (n == 0 || n == 1 || n == 2) {
+			return n;
+		}
+		int[] record = new int[n + 1];
+		record[0] = 0;
+		record[1] = 1;
+		record[2] = 2;
+		for (int i = 3; i <= n; i++) {
+			record[i] = record[i - 1] + record[i - 2];
+		}
+		return record[n];
+	}
+}
